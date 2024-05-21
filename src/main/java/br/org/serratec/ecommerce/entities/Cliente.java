e8bc755136c87f8fe39104e9eaa16276adc4867e
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-05-21 11:51:01 -0300

    Create entities Cliente and Endereco

---
+++ src/main/java/br/org/serratec/ecommerce/entities/Cliente.java
@@ -0,0 +1,97 @@
+package br.org.serratec.ecommerce.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Integer idCliente;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "nome_completo")
+	private String nomeCompleto;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+	
+	@OneToOne
+	@JoinColumn(name = "id_cliente")
+	private Endereco endereco;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+}
